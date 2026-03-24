d016e382deec75059f6323b9e54b69af50197431
jeongjaewoong <dixk3458@naver.com>
2026-03-24 15:44:01 +0900

    Landing 페이지 edit

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,3 +1,28 @@
+import Container from '../../components/layout/Container';
+import FeaturesSection from './components/FeaturesSection';
+import HeroSection from './components/HeroSection';
+import SecuritySection from './components/SecuritySection';
+import TeamSection from './components/TeamSection';
+
 export default function Landing() {
-  return <div></div>;
+  return (
+    <div>
+      <Container className="py-4">
+        <HeroSection />
+      </Container>
+      <div className="bg-gray-50/50 w-full">
+        <Container className="py-4">
+          <FeaturesSection />
+        </Container>
+      </div>
+      <Container>
+        <SecuritySection />
+      </Container>
+      <div className="bg-gray-50/50 w-full">
+        <Container className="py-4">
+          <TeamSection />
+        </Container>
+      </div>
+    </div>
+  );
 }
